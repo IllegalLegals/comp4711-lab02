eb9e48c4b58b34191dc42b22d6e94162289f3be0
Johnny <johnnymarin2011@gmail.com>
2016-01-13 14:30:12 -0800

    Connected to the database, adjusted method for getting the images that are displayed on both the home and gallery, finished Basic03 tutorial.

---
+++ application/models/Images.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jojoman46
+ * Date: 2016-01-13
+ * Time: 1:47 PM
+ */
+class Images extends CI_Model {
+
+    // constructor (a good practice)
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    // return all images, descending order by post date
+    function all()
+    {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+
+    // return just the 3 newest images
+    function newest()
+    {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+}
---
+++ application/views/_cell.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jojoman46
+ * Date: 2016-01-13
+ * Time: 1:52 PM
+ */
+
+?>
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+    <img src="./data/thumb/{filename}"/>
+</a>
